b6f613593ce8ded5bfabb07c1ff928af6b65787e
lazaro.silva <lazaro.silva@datainfo.inf.br>
2023-11-13 21:16:53 +0000

    limpando código

--- src/main/java/com/github/lazaro/agendaapi/config/WebConfig.java
+++ src/main/java/com/github/lazaro/agendaapi/config/WebConfig.java
@@ -11,8 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "PATCH");// <- assim permite de qualquer origem,
-                                                                                 // troque "/**" pelo seu dominio por
-                                                                                 // exemplo "http://meudominio.com"
+        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "PATCH");
     }
 }
